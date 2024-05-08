09e2128dc695e9b1aa27250981bce239dd596231
gyewdshuijk <gs1iloveoof@hotmail.com>
2024-05-08 17:56:03 +0700

    demo project

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function isPrime(n) {
-  if (n <= 1) return false;
-  if (n <= 3) return true;
-  if (n % 2 === 0 || n % 3 === 0) return false;
-  let i = 5;
-  while (i * i <= n) {
-    if (n % i === 0 || n % (i + 2) === 0) return false;
-    i += 6;
+const pancakeSort = (arr) => {
+  const flip = (arr, k) => {
+    let i = 0;
+    while (i < k / 2) {
+      [arr[i], arr[k - i]] = [arr[k - i], arr[i]];
+      i++;
+    }
+  };
+  const findMaxIndex = (arr, n) => {
+    let maxIndex = 0;
+    for (let i = 0; i < n; i++) {
+      if (arr[i] > arr[maxIndex]) {
+        maxIndex = i;
+      }
+    }
+    return maxIndex;
+  };
+  let currentSize = arr.length;
+  while (currentSize > 1) {
+    const maxIndex = findMaxIndex(arr, currentSize);
+    if (maxIndex !== currentSize - 1) {
+      flip(arr, maxIndex);
+      flip(arr, currentSize - 1);
+    }
+    currentSize--;
   }
-  return true;
-}
+  return arr;
+};
